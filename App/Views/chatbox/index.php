e54ef63c65386e4053f2cc89b7c135e4ca76c334
Giovane Giraldeli Pecora <giovane.pecora@shapeness.com.br>
2017-09-11 17:25:50 -0300

    Melhorias no layout

--- App/Views/chatbox/index.php
+++ App/Views/chatbox/index.php
@@ -9,26 +9,27 @@
         <script type="text/javascript" language="javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </head>
     <body>
-        <div class="container">
-            <div class="row justify-content-md-center">
-                <div class="col-sm-3 col-sm-offset-4 col-md-3 col-md-offset-4 col-lg-3 col-lg-offset-4  col-xl-3 col-xl-offset-4 tituloChat">
-                    Conversa
+        <div class="vertical-center">
+            <div class="container">
+                <div class="row justify-content-md-center">
+                    <div class="col-sm-12 col-sm-offset-4 col-md-12 col-md-offset-4 col-lg-6 col-lg-offset-4  col-xl-6 col-xl-offset-4 tituloChat">
+                        Conversa
+                    </div>
+                </div>
+                <div class="row justify-content-md-center">
+                    <div class="col-sm-12 col-sm-offset-4 col-md-12 col-md-offset-4 col-lg-6 col-lg-offset-4  col-xl-6 col-xl-offset-4 frame">
+                        <ul></ul>
+                        <div>
+                            <div class="msj-rta macro" style="margin:auto">                        
+                                <div class="text text-r" style="background:whitesmoke !important">
+                                    <input class="mytext" placeholder="Type a message"/>
+                                </div> 
+                            </div>
+                        </div>
+                    </div>        
                 </div>
             </div>
-            <div class="row justify-content-md-center">
-                <div class="col-sm-3 col-sm-offset-4 col-md-3 col-md-offset-4 col-lg-3 col-lg-offset-4  col-xl-3 col-xl-offset-4 frame">
-                    <ul></ul>
-                    <div>
-                        <div class="msj-rta macro" style="margin:auto">                        
-                            <div class="text text-r" style="background:whitesmoke !important">
-                                <input class="mytext" placeholder="Type a message"/>
-                            </div> 
-                        </div>
-                    </div>
-                </div>        
             </div>
-        </div>
-        
     </body>
     <script type="text/javascript" language="javascript" src="./public/js/funcoes.js"></script>
 </html>
